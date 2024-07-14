c6ca6be312acb2735930bf368160a0ec72c6fd5f
Storcale <105071746+storcale@users.noreply.github.com>
2024-07-14 16:18:06 +0200

    things that will get deleted anyways

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
                 .setName('ping')
                 .setDescription('Replies with Pong!');
-      export async function run(interaction) {
+      export async function run({interaction, client, handler}) {
                 await interaction.reply('Pong!');
         };
         
