ec0983cf85dc8ee37f01dd8efc2df3dbad693a46
FilipoMC <78819140+FilipoMC@users.noreply.github.com>
2024-07-14 21:24:29 +0200

    update CustomCommandOptions interface

--- src/types/CustomCommandOptions.ts
+++ src/types/CustomCommandOptions.ts
@@ -2,6 +2,6 @@ import { CommandOptions } from "commandkit";
 import { Snowflake } from "discord.js";
 
 export default interface CustomCommandOptions extends CommandOptions {
-   requiredRoles?: {roles: Array<Snowflake>, areAllRequired: boolean};
+   requiredRoles?: {roles: Array<Snowflake>, areAllRequired?: boolean};
    skipCategoryPerms?: boolean;
 }
