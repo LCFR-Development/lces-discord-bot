3c7d2b084793791c97136d59407fd52b2e4d58f8
Storcale <105071746+storcale@users.noreply.github.com>
2024-10-05 01:22:54 +0200

    Check-duty command
    it works

--- src/commands/rcrp/check-duty.ts
+++ src/commands/rcrp/check-duty.ts
@@ -1,23 +1,47 @@
 import { SlashCommandProps } from "commandkit";
-import { SlashCommandBuilder,GuildMember } from "discord.js";
+import { SlashCommandBuilder,GuildMember, EmbedBuilder, Colors, Emoji } from "discord.js";
 import { MEmployee } from "../../schemas/employees/employee";
 import { getConfig} from "../../config";
 import getCommandFailedToRunEmbed from "../../utils/getCommandFailedToRunEmbed";
 import erlc from "erlc-api"
+import axios from "axios";
+import { RobloxUserFromID } from "../../types/RobloxUserFromID";
 
-function useAPI(robloxID: string){
-    const useApi = async () => {
-        const apiKey = process.env.server_api_key as string;
+const client = new erlc.Client({globalToken: "", });
+client.config();
+
+const mainEmbed = new EmbedBuilder();
+
+interface infoType {
+    playerInfo: FixedServerPlayer;
+    vehicles: fixedVehiclesLogs;
+}
+interface FixedServerPlayer extends erlc.ServerPlayer{
+    Callsign: string | undefined;
+    Team: string;
+}
+interface fixedVehiclesLogs extends erlc.VehiclesLog{
+    Name: string | undefined;
+}
+
+    const useApi = async (robloxName:string): Promise<false | infoType> => {
+        const apiKey = process.env.SERVER_API_KEY as string;
         const players = await erlc.getPlayers(apiKey);
         const vehicles = await erlc.getVehicles(apiKey);
-        const foundPlayer = players.find(players => players.Player === robloxID);
+        const foundPlayer = players.find(players => players.Player.startsWith(robloxName));
+        const foundVehicles = vehicles.find(vehicles => vehicles.Owner === robloxName)
         if(!foundPlayer){return false}
+        const info: infoType = {
+            playerInfo: foundPlayer,
+            vehicles: foundVehicles
+        }
+        return info
     };
-}
+
 
 export const data = new SlashCommandBuilder()
     .setName("check-duty")
-    .setDescription("Check if an employee is playing as FD")
+    .setDescription("Check an employee's in-game info")
     .addUserOption(o => o
         .setName("employee")
         .setDescription("The employee to search in-game")
@@ -25,6 +49,7 @@ export const data = new SlashCommandBuilder()
     )
 
 export async function run({interaction, client}: SlashCommandProps) {
+    await interaction.deferReply({ephemeral: true});
     const config = getConfig(interaction);
     const employee = interaction.options.getMember("employee") as GuildMember;
 
@@ -32,12 +57,31 @@ export async function run({interaction, client}: SlashCommandProps) {
     try {
         const mainEmployee = await MEmployee.findOne({discordID: employee.user.id});
         if (!mainEmployee) {
-          await interaction.editReply({embeds: [getCommandFailedToRunEmbed("Employee not found.")]})
-          return;
+            await interaction.editReply({embeds: [getCommandFailedToRunEmbed("Employee not found.")]})
+            return;
         }
         const robloxID = mainEmployee.robloxID
+        const robloxMemberApiResponce = await axios.get<RobloxUserFromID>(`https://users.roblox.com/v1/users/${robloxID}`);
+        const robloxMember: RobloxUserFromID = robloxMemberApiResponce.data;
+        const robloxName = robloxMember.name
+        const info = await useApi(robloxName)
+
+        mainEmbed.setTitle(`${robloxMember.name} in-game information`);
+
+        if(info == false){
+            await interaction.editReply({embeds: [getCommandFailedToRunEmbed(`Could not find the employee in-game.`)]});
+        }else{
+            mainEmbed.setColor(config.colors.mainEmbedColor);
+            mainEmbed.setDescription(
+                `**Team:** ${info.playerInfo.Team}\n`+
+                `**Callsign:** ${info.playerInfo.Callsign}\n`+
+                `**Vehicles:** ${info.vehicles.Name}\n`
+            )
+        }
     } catch (error) {
         await interaction.editReply({embeds: [getCommandFailedToRunEmbed(`There was an error while executing this command.`)]});
+        console.log(error)
         return;
-     }
+    }
+    await interaction.editReply({embeds: [mainEmbed]});
 }
