baabf732a874ad4d359f55eb03e936ede79b2133
Filipo <filgreen@pm.me>
2024-12-15 14:37:17 +0000

    Comment fix
    Small comment update

--- src/utils/getMessageLoadingEmbed.ts
+++ src/utils/getMessageLoadingEmbed.ts
@@ -4,7 +4,6 @@ import botConfig from "../config/botConfig";
 /**
  * 
  * @param message The description (it will be put after the loading emoji)
- * @param config The config for the server
  * @returns Embed ready to be sent
  */
 export default function(message: string): EmbedBuilder {
