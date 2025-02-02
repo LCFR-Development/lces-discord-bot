c3d9c49ed5ebbade2258aab5c2b9cf8c1453077b
Filipo <filgreen@pm.me>
2025-02-02 22:28:44 +0100

    Fix ESLint config
    Made ESLint ignore some directories and files

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  includeIgnoreFile(gitignorePath),
+  {files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["**/dist/*"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
